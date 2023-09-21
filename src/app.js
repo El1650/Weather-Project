f4d0457d56241bf195523b9d187b4dda677f41b3
Elena G.Mandefro <elamandefro@gmail.com>
2023-09-21 15:15:38 +0530

    index.html

--- src/app.js
+++ src/app.js
@@ -27,18 +27,22 @@ let dt = d[now.getDay()];
 dayTime.innerHTML = `${dt} ${hr}:${min}`;
 
 //integrating the API to change the temp
-
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   let c = document.querySelector("#city").value;
-  let apiKey = "01efd6f69c452a0343e0f052f247c28f";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${c}&units=metric`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
+  searchCity(c);
 });
+
+function searchCity(city) {
+  let apiKey = "01efd6f69c452a0343e0f052f247c28f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
 
 function showTemp(response) {
   let f = document.querySelector("#deg-far");
   let c = document.querySelector("#deg-cel");
+  document.querySelector("h2").innerHTML = response.data.name;
   document.querySelector("#hum").innerHTML = Math.round(
     response.data.main.humidity
   );
@@ -63,6 +67,7 @@ function showTemp(response) {
   });
 }
 
+//Using Geolocation API to get the GPS coordinates and display the current city and temp
 let curr = document.querySelector("#current");
 curr.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -70,7 +75,8 @@ curr.addEventListener("submit", function (event) {
 });
 
 function searchPos(position) {
-  let apiKeyy = "01efd6f69c452a0343e0f052f247c28f";
+  let apiKeyy = "5f465a71fc837e1674165a8ed3b775f1";
   let apiUrll = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKeyy}&units=metric`;
   axios.get(apiUrll).then(showTemp);
 }
+searchCity("New York");
